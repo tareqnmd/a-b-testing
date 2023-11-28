065a1234b7563f2cd1b73dfb8ed17d32e1f1a406
Tareq <tareqnmd@gmail.com>
2023-11-28 14:59:44 +0600

    update swanky

---
+++ swankybadger/bf-collection-low-price.js
@@ -0,0 +1,36 @@
+const low_price_html = `
+<span class="low-price-html">🔥 Our Lowest Price ever!</span>
+`;
+
+const get_low_price = (item, current, was) => {
+	try {
+		const current_price_number = parseFloat(
+			current.innerText.replace(/\$/g, '')
+		);
+		const was_price_number = parseFloat(was.innerText.replace(/\$/g, ''));
+		const product_details = item.querySelector('.product-details');
+		if (product_details && was_price_number - current_price_number >= 70) {
+			product_details.insertAdjacentHTML('beforebegin', low_price_html);
+		}
+	} catch (error) {}
+};
+
+setInterval(() => {
+	const items = [
+		...document.querySelectorAll(
+			'.product-list.product-list--collection .column'
+		),
+	];
+	items?.map((item) => {
+		const current_price = item.querySelector('.current_price .money .money');
+		const was_price = item.querySelector('.was_price .money .money');
+		if (
+			item &&
+			current_price &&
+			was_price &&
+			!item.querySelector('.low-price-html')
+		) {
+			get_low_price(item, current_price, was_price);
+		}
+	});
+}, 10);
