ce9b6e48c35d6186ff54d65ae4172eafd5c4609f
Tareq <tareqnmd@gmail.com>
2023-12-01 00:05:56 +0600

    new clock

---
+++ swankybadger/bf-clock-header.js
@@ -0,0 +1,87 @@
+/* CUSTOM CODE */
+const clock_html = `
+<style>
+	.header-clock {
+		background: #0f0f0f;
+		padding: 12px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 40px;
+		width: 100%;
+	}
+	.header-clock h2 {
+		color: #fff;
+		font-family: Oswald;
+		font-size: 32px;
+		line-height: 32px;
+		font-style: normal;
+		font-weight: 700;
+        margin: 0;
+	}
+	.header-clock .clock-timer {
+		display: inline-flex;
+		align-items: center;
+		gap: 12px;
+	}
+	.header-clock .clock-area {
+		border-radius: 6px;
+		background: #03a196;
+		display: flex;
+		padding: 8px 12px;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
+	}
+	.header-clock .clock-area span {
+		color: #fff;
+		font-family: Roboto;
+		font-size: 24px;
+		line-height: 24px;
+		font-style: normal;
+		font-weight: 900;
+	}
+	.header-clock .clock-area small {
+		color: #fbfbfb;
+		leading-trim: both;
+		text-edge: cap;
+		font-family: Roboto;
+		font-size: 10px;
+		line-height: 10px;
+		font-style: normal;
+		font-weight: 500;
+	}
+</style>
+<div class="header-clock">
+	<h2>CYBER WEEK SALE ENDS IN</h2>
+	<div class="clock-timer">
+		<div class="clock-area">
+			<span>00</span>
+			<small>DAYS</small>
+		</div>
+		<div class="clock-area">
+			<span>00</span>
+			<small>HRS</small>
+		</div>
+		<div class="clock-area">
+			<span>00</span>
+			<small>MIN</small>
+		</div>
+		<div class="clock-area">
+			<span>00</span>
+			<small>SEC</small>
+		</div>
+	</div>
+</div>
+`;
+const header_clock_int = setInterval(() => {
+const header = document.querySelector('.header .top-bar'); 
+	if (
+		header &&
+		!document.querySelector('.header-clock') 
+	) {
+		header.insertAdjacentHTML('beforebegin', clock_html);
+		clearInterval(header_clock_int);
+	}
+}, 10);
