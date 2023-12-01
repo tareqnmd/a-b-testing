b8b93192504c73a6124b1daf96c0b266f66b04a1
Tareq <tareqnmd@gmail.com>
2023-12-01 18:55:54 +0600

    up

--- swankybadger/cyber-clock-header.js
+++ swankybadger/cyber-clock-header.js
@@ -17,8 +17,8 @@ const style = `
 		line-height: 32px;
 		font-style: normal;
 		font-weight: 700;
-        margin: 0;
-        letter-spacing:1px;
+		margin: 0;
+		letter-spacing: 1px;
 	}
 	.header-clock .clock-timer {
 		display: inline-flex;
@@ -45,47 +45,57 @@ const style = `
 	}
 	.header-clock .clock-area small {
 		color: #fbfbfb;
-		leading-trim: both;
-		text-edge: cap;
 		font-family: Roboto;
 		font-size: 10px;
 		line-height: 10px;
 		font-style: normal;
 		font-weight: 500;
 	}
-    @media only screen and (max-width:700px){
-        .header-clock {
-            gap: 20px;
-        }
-        .header-clock h2 {
-            font-size: 16px;
-            line-height: 16px;
-        }
-        .header-clock .clock-area {
-            padding: 4px 8px;
-            gap: 2px;
-        }
-        .header-clock .clock-area span {
-            font-size: 12px;
-            line-height: 12px;
-        }
-        .header-clock .clock-area small {
-            font-size: 8px;
-            line-height: 8px;
-        }
-    }
-    @media only screen and (max-width:420px){
-        .header-clock {
-            gap: 10px;
-        }
-        .header-clock h2 {
-            font-size: 14px;
-            line-height: 14px;
-        }
-        .header-clock .clock-timer {
-            gap: 6px;
-        }
-    }
+	.header-clock:nth-of-type(2) {
+		display: none;
+	}
+
+	@media only screen and (max-width: 798px) {
+		.header-clock:nth-of-type(1) {
+			display: none;
+		}
+		.header-clock:nth-of-type(2) {
+			display: flex;
+		}
+	}
+	@media only screen and (max-width: 700px) {
+		.header-clock {
+			gap: 20px;
+		}
+		.header-clock h2 {
+			font-size: 16px;
+			line-height: 16px;
+		}
+		.header-clock .clock-area {
+			padding: 4px 8px;
+			gap: 2px;
+		}
+		.header-clock .clock-area span {
+			font-size: 12px;
+			line-height: 12px;
+		}
+		.header-clock .clock-area small {
+			font-size: 8px;
+			line-height: 8px;
+		}
+	}
+	@media only screen and (max-width: 420px) {
+		.header-clock {
+			gap: 10px;
+		}
+		.header-clock h2 {
+			font-size: 14px;
+			line-height: 14px;
+		}
+		.header-clock .clock-timer {
+			gap: 6px;
+		}
+	}
 </style>
 `;
 const clock_html = `
