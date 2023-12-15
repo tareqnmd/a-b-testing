5bb6be10624f79141a46b0c02cb7fe8cbf6b9162
Tareq <tareqnmd@gmail.com>
2023-12-15 15:30:33 +0600

    update

--- back in motion/contact.js
+++ back in motion/contact.js
@@ -339,11 +339,96 @@ const usp_html = `
 </div>
 `;
 
+const five_star = `
+<svg
+	width="69"
+	height="12"
+	viewBox="0 0 69 12"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M6.59564 0L7.94273 4.1459H12.302L8.77527 6.7082L10.1224 10.8541L6.59564 8.2918L3.06893 10.8541L4.41601 6.7082L0.889303 4.1459H5.24856L6.59564 0Z"
+		fill="#F59E0B"
+	/>
+	<path
+		d="M20.5956 0L21.9427 4.1459H26.302L22.7753 6.7082L24.1224 10.8541L20.5956 8.2918L17.0689 10.8541L18.416 6.7082L14.8893 4.1459H19.2486L20.5956 0Z"
+		fill="#F59E0B"
+	/>
+	<path
+		d="M34.5956 0L35.9427 4.1459H40.302L36.7753 6.7082L38.1224 10.8541L34.5956 8.2918L31.0689 10.8541L32.416 6.7082L28.8893 4.1459H33.2486L34.5956 0Z"
+		fill="#F59E0B"
+	/>
+	<path
+		d="M48.5956 0L49.9427 4.1459H54.302L50.7753 6.7082L52.1224 10.8541L48.5956 8.2918L45.0689 10.8541L46.416 6.7082L42.8893 4.1459H47.2486L48.5956 0Z"
+		fill="#F59E0B"
+	/>
+	<path
+		d="M62.5956 0L63.9427 4.1459H68.302L64.7753 6.7082L66.1224 10.8541L62.5956 8.2918L59.0689 10.8541L60.416 6.7082L56.8893 4.1459H61.2486L62.5956 0Z"
+		fill="#F59E0B"
+	/>
+</svg>
+`;
+
+const client_html = `
+<div class="review">
+	<div>
+		<div class="rev-info">
+			<img
+				src="https://i.ibb.co/kxxhJBx/Ellipse-110.png"
+				alt="user-img"
+			/>
+			<strong>Brooklyn Simmons</strong>
+			${five_star}
+		</div>
+		<p>
+			“Porem ipsum dolor sit amet consectetur. Metus volutpat pulvinar pulvinar
+			pellentesque accumsan. Risus porta in pellentesque vulputate egestas
+			integer proin. Tincidunt et adipiscing pharetra mi.”
+		</p>
+		<small>4 Jun 2023</small>
+	</div>
+	<div>
+		<div class="rev-info">
+			<img
+				src="https://i.ibb.co/kxxhJBx/Ellipse-110.png"
+				alt="user-img"
+			/>
+			<strong>Brooklyn Simmons</strong>
+			${five_star}
+		</div>
+		<p>
+			“Porem ipsum dolor sit amet consectetur. Metus volutpat pulvinar pulvinar
+			pellentesque accumsan. Risus porta in pellentesque vulputate egestas
+			integer proin. Tincidunt et adipiscing pharetra mi.”
+		</p>
+		<small>4 Jun 2023</small>
+	</div>
+	<div>
+		<div class="rev-info">
+			<img
+				src="https://i.ibb.co/kxxhJBx/Ellipse-110.png"
+				alt="user-img"
+			/>
+			<strong>Brooklyn Simmons</strong>
+			${five_star}
+		</div>
+		<p>
+			“Porem ipsum dolor sit amet consectetur. Metus volutpat pulvinar pulvinar
+			pellentesque accumsan. Risus porta in pellentesque vulputate egestas
+			integer proin. Tincidunt et adipiscing pharetra mi.”
+		</p>
+		<small>4 Jun 2023</small>
+	</div>
+</div>
+`;
+
 const interval = setInterval(() => {
 	const main = document.querySelector('#main');
 	const head = document.querySelector('head');
 	if (main && head && !document.querySelector('.benefit-area')) {
 		head.insertAdjacentHTML('beforeend', style);
+		main.insertAdjacentHTML('beforeend', client_html);
 		main.insertAdjacentHTML('afterend', benefit_html);
 		clearInterval(interval);
 	}
