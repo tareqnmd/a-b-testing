7018e2d6b9cf79f2eb7b17156b2d68e0d9fce661
Tareq <tareqnmd@gmail.com>
2023-12-15 14:47:18 +0600

    update

--- back in motion/contact.js
+++ back in motion/contact.js
@@ -1,5 +1,31 @@
 const style = `
 <style>
+    .fusion-chk {
+        padding-top: 0!important;
+    }
+    .fusion-chk .usp{
+        background: #EBF2F6;
+        display: flex;
+        padding: 20px 40px;
+        justify-content: center;
+        align-items: center;
+        margin-bottom:20px!important;
+        gap:30px;
+    }
+    .fusion-chk .usp > div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+    }
+    .fusion-chk .usp span{
+        color: #1F2937;
+        font-family: Inter;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 20px;
+    }
     .response-info{
         display: flex;
         align-items:center;
@@ -181,6 +207,120 @@ const response_html = `
 </div>
 `;
 
+const usp_html = `
+<div class="usp">
+	<div>
+		<svg
+			width="41"
+			height="40"
+			viewBox="0 0 41 40"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<rect
+				x="0.5"
+				width="40"
+				height="40"
+				rx="20"
+				fill="white"
+			/>
+			<path
+				d="M23.5783 8.57391C19.1174 8.57391 15.4913 12.2522 15.4913 16.7391C15.4913 18.6435 16.1435 20.3652 17.2131 21.7739L9.54349 29.5478C9.10001 30.0174 9.10001 30.7478 9.54349 31.1913C9.77827 31.4261 10.0652 31.5304 10.3522 31.5304C10.6652 31.5304 10.9522 31.4261 11.187 31.1913L18.8565 23.4174C20.187 24.3826 21.8044 24.9304 23.5522 24.9304C28.0131 24.9304 31.6391 21.2522 31.6391 16.7652C31.6391 12.2783 28.0391 8.57391 23.5783 8.57391ZM23.5783 22.8696C20.2652 22.8696 17.5783 20.1304 17.5783 16.7652C17.5783 13.4 20.2652 10.6609 23.5783 10.6609C26.8913 10.6609 29.5783 13.4 29.5783 16.7652C29.5783 20.1304 26.8913 22.8696 23.5783 22.8696ZM27.6217 16.8696C27.5696 17.313 27.1783 17.6522 26.7348 17.6522C26.6826 17.6522 26.6565 17.6522 26.6044 17.6522C26.1087 17.5739 25.7696 17.1304 25.8217 16.6348C26.0044 15.2783 24.4913 14.5739 24.4131 14.5478C23.9696 14.3391 23.7609 13.7913 23.9696 13.3478C24.1783 12.9043 24.7 12.6957 25.1696 12.8783C25.2739 12.9043 27.987 14.1565 27.6217 16.8696ZM25.7174 18.8522C25.9 19.0348 26.0304 19.3217 26.0304 19.5826C26.0304 19.8696 25.9261 20.1304 25.7174 20.313C25.5348 20.4957 25.2478 20.6261 24.987 20.6261C24.7261 20.6261 24.4391 20.5217 24.2565 20.313C24.0739 20.1304 23.9435 19.8435 23.9435 19.5826C23.9435 19.3217 24.0478 19.0348 24.2565 18.8522C24.4391 18.6696 24.7261 18.5391 24.987 18.5391C25.2478 18.5391 25.5087 18.6435 25.7174 18.8522Z"
+				fill="#003A5D"
+			/>
+		</svg>
+		<span>Get Back to Being Active</span>
+	</div>
+	<div>
+		<svg
+			width="41"
+			height="40"
+			viewBox="0 0 41 40"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<rect
+				x="0.5"
+				width="40"
+				height="40"
+				rx="20"
+				fill="white"
+			/>
+			<g clip-path="url(#clip0_9_163)">
+				<path
+					d="M25.8625 21.2375H15.1375C14.875 21.2375 14.6125 21.35 14.4625 21.575C14.3125 21.8 14.2375 22.0625 14.3125 22.325C15.1 25.1 17.65 27.05 20.5 27.05C23.425 27.05 25.9 25.175 26.6875 22.325C26.7625 22.0625 26.725 21.8 26.5375 21.575C26.3875 21.35 26.125 21.2375 25.8625 21.2375ZM20.5 25.325C18.775 25.325 17.2375 24.3875 16.375 22.925H24.625C23.8 24.3875 22.2625 25.325 20.5 25.325Z"
+					fill="#003A5D"
+				/>
+				<path
+					d="M20.5 8.3C14.05 8.3 8.79999 13.55 8.79999 20C8.79999 26.45 14.05 31.7375 20.5375 31.7375C27.025 31.7375 32.275 26.4875 32.275 20C32.275 13.5125 26.95 8.3 20.5 8.3ZM20.5 30.05C14.95 30.05 10.45 25.55 10.45 20C10.45 14.45 14.9875 9.9875 20.5 9.9875C26.0125 9.9875 30.55 14.4875 30.55 20.0375C30.55 25.5875 26.05 30.05 20.5 30.05Z"
+					fill="#003A5D"
+				/>
+				<path
+					d="M16 18.05C16.8284 18.05 17.5 17.3784 17.5 16.55C17.5 15.7216 16.8284 15.05 16 15.05C15.1716 15.05 14.5 15.7216 14.5 16.55C14.5 17.3784 15.1716 18.05 16 18.05Z"
+					fill="#003A5D"
+				/>
+				<path
+					d="M25 18.05C25.8284 18.05 26.5 17.3784 26.5 16.55C26.5 15.7216 25.8284 15.05 25 15.05C24.1716 15.05 23.5 15.7216 23.5 16.55C23.5 17.3784 24.1716 18.05 25 18.05Z"
+					fill="#003A5D"
+				/>
+			</g>
+			<defs>
+				<clipPath id="clip0_9_163">
+					<rect
+						width="24"
+						height="24"
+						fill="white"
+						transform="translate(8.5 8)"
+					/>
+				</clipPath>
+			</defs>
+		</svg>
+		<span>Stop Worrying</span>
+	</div>
+	<div>
+		<svg
+			width="40"
+			height="40"
+			viewBox="0 0 40 40"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<rect
+				width="40"
+				height="40"
+				rx="20"
+				fill="white"
+			/>
+			<g clip-path="url(#clip0_18_8)">
+				<path
+					d="M8.5 30.52C10.1425 30.5439 11.7715 30.2201 13.28 29.57C15.14 30.1515 17.0719 30.4712 19.02 30.52C21.89 30.52 29.54 29.57 29.54 29.57L30.54 28.57V22.87C30.54 18.09 27.67 9.48 27.67 9.48H22.85L20.93 11.39V13.3L22.85 15.22H24.76"
+					stroke="#003A5D"
+					stroke-width="1.91"
+					stroke-miterlimit="10"
+				/>
+				<path
+					d="M24.76 12.35V16.17C24.053 18.6605 23.7161 21.2415 23.76 23.83C23.6922 22.5829 23.1709 21.4036 22.2943 20.514C21.4176 19.6244 20.246 19.086 19 19C17.2225 19.0637 15.5295 19.775 14.24 21C12.6459 19.6359 10.5967 18.9219 8.5 19"
+					stroke="#003A5D"
+					stroke-width="1.91"
+					stroke-miterlimit="10"
+				/>
+			</g>
+			<defs>
+				<clipPath id="clip0_18_8">
+					<rect
+						width="24"
+						height="24"
+						fill="white"
+						transform="translate(8 8)"
+					/>
+				</clipPath>
+			</defs>
+		</svg>
+		<span>Get to the Root Cause</span>
+	</div>
+</div>
+`;
+
 const interval = setInterval(() => {
 	const main = document.querySelector('#main');
 	const head = document.querySelector('head');
@@ -193,9 +333,16 @@ const interval = setInterval(() => {
 
 setInterval(() => {
 	const button = document.querySelector('.gform-theme-button.button');
+	const fusion = document.querySelector(
+		'.fusion-builder-column-7 .fusion-column-wrapper.fusion-column-has-shadow'
+	);
 	if (button && !document.querySelector('.btn-chk')) {
 		button.classList.add('btn-chk');
 		button.innerText = 'Request my callback';
 		button.insertAdjacentHTML('afterend', response_html);
+	}
+	if (fusion && !document.querySelector('.fusion-chk')) {
+		fusion.classList.add('fusion-chk');
+		fusion.insertAdjacentHTML('afterbegin', usp_html);
 	}
 }, 10);
