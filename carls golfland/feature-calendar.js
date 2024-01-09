c8bf86ce99b035622497620ec88317f9959f5b5b
Tareq <tareqnmd@gmail.com>
2024-01-09 14:26:15 +0600

    update feature cal

--- carls golfland/feature-calendar.js
+++ carls golfland/feature-calendar.js
@@ -1,15 +1,28 @@
-const img_link = '';
-const url = '';
+const img_link =
+	'https://carlsgolfland.s3.us-east-2.amazonaws.com/cgl-launch-cal.png';
+const url = 'https://www.carlsgolfland.com/launch_calendar';
 
 const feature_calendar = `
 <li>
-  <a href=${url}> <img src=${img_link} alt='calendar icon'> Launch Calendar</a>
+  <a class='cal-link-li' href=${url}> 
+    <span class='cal-ico-rel'>
+      <img src=${img_link} alt='calendar icon'>
+      <small class='cal-ico'></small>
+    </span> 
+    Launch Calendar
+  </a>
 </li>
 `;
 
 const feature_calendar_mobile = `
-<li class="level0 nav-10 category-item last level-top gift-cards ui-menu-item">
-  <a href=${url}> <img src=${img_link} alt='calendar icon'> Launch Calendar</a>
+<li class="level0 nav-11 category-item last level-top gift-cards ui-menu-item">
+  <a class='cal-link-li' href=${url}> 
+    <span class='cal-ico-rel'>
+      <img src=${img_link} alt='calendar icon'>
+      <small class='cal-ico'></small>
+    </span> 
+    Launch Calendar
+  </a>
 </li>
 `;
 
@@ -27,3 +40,30 @@ const feature_int = setInterval(() => {
 		clearInterval(feature_int);
 	}
 }, 10);
+
+{
+	/* <style>
+.header-panel-right .header.links li:nth-of-type(3){
+  display:none;
+}
+.cal-link-li{
+  display:flex!important;
+  gap:4px;
+}
+.cal-ico-rel{
+  position:relative;
+  display:block;
+  height:20px;
+  width:20px;
+}
+.cal-ico{
+  position:absolute;
+  background:red;
+  width:10px;
+  height:10px;
+  top:-2px;
+  right:-2px;
+  border-radius:50%;
+}
+</style> */
+}
