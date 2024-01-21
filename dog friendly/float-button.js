a49036a46dbaaa5b52471e38e5baf67c8db92262
Tareq <tareqnmd@gmail.com>
2024-01-22 00:16:14 +0600

    float btn added

--- dog friendly/float-button.js
+++ dog friendly/float-button.js
@@ -1,1 +1,10 @@
-
+window.addEventListener('scroll', function () {
+	const btn_area = document.querySelector('.add-to-cart-custom-btn');
+	const btn_wrapper = document.querySelector('.add-to-cart-wrapper');
+	const topOffset = btn_area.getBoundingClientRect().bottom;
+	if (topOffset <= 0) {
+		btn_wrapper.classList.add('new-float-btn');
+	} else {
+		btn_wrapper.classList.remove('new-float-btn');
+	}
+});
