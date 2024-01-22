206e0b071d0c75696325534e2469f48eb4c7ed2e
Tareq <tareqnmd@gmail.com>
2024-01-22 10:21:01 +0600

    miss amara code updated

--- missamara/pdp-social-proof.js
+++ missamara/pdp-social-proof.js
@@ -11,19 +11,7 @@ const style = `
 
 const pdp_html = `
 <span class="pdp-message-area">
-	<svg
-		stroke="currentColor"
-		fill="currentColor"
-		stroke-width="0"
-		viewBox="0 0 512 512"
-		height="1em"
-		width="1em"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z"
-		></path>
-	</svg>
+	<img src="https://cdn.shopify.com/s/files/1/2594/4244/files/glasses.png" alt="">
 	<span>
         2 people are viewing this product!
     </span>
@@ -35,8 +23,13 @@ const message_int = setInterval(() => {
 	const cart_btn = document.querySelector(
 		'.input-row.quantity-submit-row.wishlist-productpage-icon.available'
 	);
+	const process_check = document.querySelector('#top_product ');
 
-	if (cart_btn && !document.querySelector('.pdp-message-area')) {
+	if (
+		cart_btn &&
+		process_check &&
+		!document.querySelector('.pdp-message-area')
+	) {
 		head.insertAdjacentHTML('beforeend', style);
 		cart_btn.insertAdjacentHTML('afterend', pdp_html);
 		clearInterval(message_int);
