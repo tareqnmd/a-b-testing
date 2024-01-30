2a4f0fd121b91befd2f71172fb335ec1c4586ca6
Tareq <tareqnmd@gmail.com>
2024-01-30 15:12:50 +0600

    update

--- carls golfland/home-sec-change.js
+++ carls golfland/home-sec-change.js
@@ -10,3 +10,17 @@ const message_int = setInterval(() => {
 		clearInterval(message_int);
 	}
 }, 10);
+
+// .home-page-brands .home-page-brands-block {
+//     display: grid;
+//     grid-template-columns: repeat(10, 1fr);
+//   }
+//   .home-page-brands .home-page-brands-block .home-page-brands-item{
+//     width: 100%;
+//     max-width: 100%;
+//   }
+// @media only screen and (max-width:991px){
+//     .home-page-brands .home-page-brands-block {
+//       grid-template-columns: repeat(5, 1fr);
+//     }
+// }
