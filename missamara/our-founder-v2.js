c3026acbe6e695d6ba1bc8be5067c7a188d60de3
Tareq <tareqnmd@gmail.com>
2024-02-08 23:32:52 +0600

    miss amara up

---
+++ missamara/our-founder-v2.js
@@ -0,0 +1,83 @@
+const style = `
+<style>
+	.new-founder-section {
+    	background: #FBE6E3;
+        padding:40px 0;
+    }
+	.new-founder-section .container{
+    	display:grid;
+        grid-template-columns: 1fr 1fr;
+        gap:20px;
+    }
+	.new-founder-section span{
+    	color: #000;
+        font-family: Julee;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+	.new-founder-section p{
+        color: #333;
+        font-family: Lato;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 28px;
+    }
+	.new-founder-section p strong{
+        color: #000;
+        text-align: justify;
+        font-family: Lato;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 28px;
+    }
+	.new-founder-section .founder-info{
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+	.new-founder-section .founder-info img{
+        max-width:250px;
+        margin : 10px 0;
+    }
+    @media only screen and (max-width: 991px) {
+        .new-founder-section .container {
+            grid-template-columns: 1fr;
+        }
+    }
+</style>
+`;
+const founder_html = `
+<div class="shopify-section new-founder-section">
+	<div class="container">
+        <picture>
+            <source media="(min-width: 768px)" srcset="https://i.ibb.co/nL7jWpZ/Desktop.png">
+            <img src="https://i.ibb.co/2q72TRR/Mobile.png">
+        </picture>
+		<div class="founder-info">
+			<span>Meet Our Founder</span>
+			<img src="https://i.ibb.co/H70rkCy/founder-meet.png">
+			<p>
+                <strong>We are Aaron & Alex</strong>, co-founders plus husband and wife! Miss Amara truly is a family owned business.
+			</p>
+            <p>
+                We believe shopping for rugs online should be fun and easy. We LOVE designing beautiful rugs, but we've since learnt there are other things that drive us. Today, that's about making a difference to your life, our team, our makers and the environment - we've developed a higher purpose if you will.
+            </p>
+		</div>
+	</div>
+</div>
+`;
+
+setInterval(() => {
+	const rugs_elm = document.querySelector(
+		'#shopify-section-ss_home_recommendations_BCJFWT'
+	);
+	const head = document.querySelector('head');
+	if (rugs_elm && head && !document.querySelector('.new-founder-section')) {
+		head.insertAdjacentHTML('beforeend', style);
+		rugs_elm.insertAdjacentHTML('beforebegin', founder_html);
+	}
+}, 10);
