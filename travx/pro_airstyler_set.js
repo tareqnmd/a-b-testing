185212099ec15ee34a1c0ff1c2e609ca3220e639
Tareq <tareqnmd@gmail.com>
2024-02-24 06:49:21 +0600

    sec step

--- travx/pro_airstyler_set.js
+++ travx/pro_airstyler_set.js
@@ -57,7 +57,49 @@ const style = `
         line-height: 20px;
         color: #707070;
     }
+    .pdp-usps{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .pdp-usp{
+        display: flex;
+        align-items: center;
+        padding: 0 4px;
+        gap: 2px;
+    }
+    .pdp-usp:not(:last-child){
+        border-right: 1px solid #000;
+    }
+    .pdp-usp img{
+        width: 16px;
+    }
+    .pdp-usp span{
+        font-size: 10px;
+        line-height: 10px;
+    }
+    .btn.btn--full.add-to-cart{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 4px;
+        border-radius: 4px!important;
+        box-shadow: none!important;
+        background: #1F1F1F;
+        border: none;
+    }
     @media only screen and (min-width:768px){
+        .pdp-usp{
+            padding: 0 8px;
+            gap: 4px;
+        }
+        .pdp-usp img{
+            width: 18px;
+        }
+        .pdp-usp span{
+            font-size: 12px;
+            line-height: 12px;
+        }
         .new-usps-area .new-usps{
             gap:20px;
             grid-template-columns: repeat(3, 1fr);
@@ -178,7 +220,25 @@ const return_icon = `
 </svg>
 `;
 
-const new_elm_html = `
+const cart_icon = `
+<svg
+	width="20"
+	height="20"
+	viewBox="0 0 20 20"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M17.5 4.16667L15.8333 10H6.14726M16.6667 13.3333H6.66667L5 2.5H2.5M13.3333 4.58333H11.25M11.25 4.58333H9.16667M11.25 4.58333V6.66667M11.25 4.58333V2.5M7.5 16.6667C7.5 17.1269 7.1269 17.5 6.66667 17.5C6.20643 17.5 5.83333 17.1269 5.83333 16.6667C5.83333 16.2064 6.20643 15.8333 6.66667 15.8333C7.1269 15.8333 7.5 16.2064 7.5 16.6667ZM16.6667 16.6667C16.6667 17.1269 16.2936 17.5 15.8333 17.5C15.3731 17.5 15 17.1269 15 16.6667C15 16.2064 15.3731 15.8333 15.8333 15.8333C16.2936 15.8333 16.6667 16.2064 16.6667 16.6667Z"
+		stroke="white"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
+`;
+
+const rev_usp_html = `
 <div class="new-elm new-usps-area">
     <div class="header-info">
         <h3>WHY 100,000+ CUSTOMERS LOVE TRAVX.</h3>
@@ -204,12 +264,37 @@ const new_elm_html = `
 </div>
 `;
 
+const pdp_usp_html = `
+<div class="product-block pdp-usps">
+    <div class="pdp-usp">
+        <img src="https://i.ibb.co/3dvttp8/usp1.png" alt="" />
+        <span>Bestel nu, betaal later</span>
+    </div>
+    <div class="pdp-usp">
+        <img src="https://cdn.shopify.com/s/files/1/0682/9644/2123/files/klarna_logo.png?v=1697923808" alt="" />
+        <span>Bestel nu, betaal later</span>
+    </div>
+    <div class="pdp-usp">
+        <img src="https://cdn.shopify.com/s/files/1/0682/9644/2123/files/postnl-3-logo-png-transparent-removebg-preview.png?v=1705871635" alt="" />
+        <span>Gratis Verzending</span>
+    </div>
+</div>
+`;
+
 const interval = setInterval(() => {
-	const exist_elm = document.querySelector('.product-grid__container.grid.grid--product-images--partial');
+	const exist_elm = document.querySelector(
+		'.product-grid__container.grid.grid--product-images--partial'
+	);
+	const form_cart = document.querySelector(
+		'.product-single__meta > div:nth-of-type(2) > div:nth-of-type(8)'
+	);
+	const cart_btn = document.querySelector('#product-form__cart-submit');
 	const head = document.querySelector('head');
 	if (exist_elm && head && !document.querySelector('.new-elm')) {
 		head.insertAdjacentHTML('beforeend', style);
-		exist_elm.insertAdjacentHTML('afterend', new_elm_html);
+		cart_btn.insertAdjacentHTML('afterbegin', cart_icon);
+		exist_elm.insertAdjacentHTML('afterend', rev_usp_html);
+		form_cart.insertAdjacentHTML('afterend', pdp_usp_html);
 		clearInterval(interval);
 	}
 }, 10);
