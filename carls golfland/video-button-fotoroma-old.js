adcb3d1f7f0356afaaf51e47054282ecf5564fdc
Tareq <tareqnmd@gmail.com>
2024-03-07 17:30:07 +0600

    pop

--- carls golfland/video-button-fotoroma-old.js
+++ carls golfland/video-button-fotoroma-old.js
@@ -1,5 +1,26 @@
 const video_links = {
-	FTJ51075: 'https://www.youtube.com/embed/wCkVEAFTMQI',
+	FTJ51075:
+		'https://www.youtube.com/embed/wCkVEAFTMQI?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	FTJ53916:
+		'https://www.youtube.com/embed/hfFL3GI5hX8?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITPROV123Y:
+		'https://www.youtube.com/embed/sEiLF-iPwqY?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITPLAYGLOV20:
+		'https://www.youtube.com/embed/c1JgKw-0p8M?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITTSR3DRP:
+		'https://www.youtube.com/embed/ZIs_gcLMQVg?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITTSR2DRW:
+		'https://www.youtube.com/embed/XBuBZE9Po3s?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITTSR2DRP:
+		'https://www.youtube.com/embed/XBuBZE9Po3s?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITTSR3DR:
+		'https://www.youtube.com/embed/ZIs_gcLMQVg?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	TITTSR2DR:
+		'https://www.youtube.com/embed/XBuBZE9Po3s?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=3',
+	GFOG4GMF000042T:
+		'https://www.youtube.com/embed/sPoefnsWJa0?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
+	GFOG4MC0EF40T:
+		'https://www.youtube.com/embed/sPoefnsWJa0?autoplay=1&rel=0&enablejsapi=1&origin=https%3A%2F%2Fwww.carlsgolfland.com&widgetid=1',
 };
 
 const style = `
@@ -43,9 +64,16 @@ const product_html = `
 </div>
 `;
 
-const vid_area = (url) => `
+const vid_area = (url, title) => `
 <div class="vid-area">
-    <iframe id="vid_iframe" src=${`${url}?autoplay=1`} title="FootJoy HyperFlex White/Lime/Blue Golf Shoes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	<iframe
+		id="vid_iframe"
+		src=${url}
+		title=${title}
+		frameborder="0"
+		allowfullscreen=""
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	></iframe>
 </div>
 `;
 
@@ -63,17 +91,18 @@ const click_int = setInterval(() => {
 	const video_btn = document.querySelector('.watch-video');
 	const fotorama_stage = document.querySelector('.fotorama__stage');
 	const sku = document.querySelector('.product.attribute.sku .value').innerText;
+	const title = document.querySelector('.page-title').innerText;
 	if (video_btn && fotorama_stage) {
 		video_btn.addEventListener('click', () => {
 			video_btn.disabled = true;
 			fotorama_stage.insertAdjacentHTML(
 				'afterbegin',
-				vid_area(video_links[sku])
+				vid_area(video_links[sku], title)
 			);
 			setTimeout(() => {
 				video_btn.disabled = false;
 				document.querySelector('.vid-area').remove();
-			}, 7000);
+			}, 12000);
 		});
 		clearInterval(click_int);
 	}
