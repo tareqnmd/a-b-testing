da9dd97fe4f55d80eb9405508c933c07c7428ad1
Tareq <tareqnmd@gmail.com>
2024-03-08 07:12:38 +0600

    update

--- missamara/product-layout.js
+++ missamara/product-layout.js
@@ -6,17 +6,45 @@ const style = `
 	.product-link {
 		display: grid!important;
 		grid-template-columns: 1fr 1fr;
+		padding: 10px;
 		gap: 6px;
 	}
-	.product-link * {
+	.product-link *, .product-block .block-inner {
 		margin: 0!important;
 		padding: 0!important;
 	}
 	.product-info, .image-cont {
 		grid-column: span 2;
 	}
+	.product-block .image-cont .image-label-wrap div img {
+		width: 100%:
+	}
+	.product-link .ss-current-price {
+		text-align: right!important;
+	}
 	.product-link .inner {
 		width: 100%!important;
+	}
+	.product-link .inner .title {
+		font-family: Lato;
+		font-size: 13px;
+		font-weight: 700;
+		line-height: 16px;
+		color: #464646;
+	}
+	.product-link .ss-current-price .from {
+		color: #798086;
+		font-family: Lato;
+		font-size: 14px;
+		font-weight: 700;
+		line-height: 16px;
+	}
+	.product-link .ss-current-price .ss__price {
+		font-family: Lato;
+		font-size: 14px;
+		font-weight: 900;
+		line-height: 16px;
+		color: #000;
 	}
 </style>
 `;
