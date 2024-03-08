e1d3020dd62c6bd81e498a0fed0c275dd5c246af
Tareq <tareqnmd@gmail.com>
2024-03-08 07:32:23 +0600

    update

--- missamara/product-layout.js
+++ missamara/product-layout.js
@@ -46,6 +46,34 @@ const style = `
 		line-height: 16px;
 		color: #000;
 	}
+	#bc-sf-filter-products div div:nth-last-of-type(1) {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap:10px;
+	}
+	.ss__toolbar.ss__toolbar--top{
+		grid-column: span 3;
+	}
+	.collection-listing .product-block {
+		margin: 0!important;
+		width: 100%!important;
+	}
+	@media only screen and (max-width:991px){
+		#bc-sf-filter-products div div:nth-last-of-type(1) {
+			grid-template-columns: 1fr 1fr;
+		}
+		.ss__toolbar.ss__toolbar--top{
+			grid-column: span 2;
+		}
+	}
+	@media only screen and (max-width:540px){
+		#bc-sf-filter-products div div:nth-last-of-type(1) {
+			grid-template-columns: 1fr;
+		}
+		.ss__toolbar.ss__toolbar--top{
+			grid-column: span 1;
+		}
+	}
 </style>
 `;
 
