c3cc37f2caf7711f87fcf78fac52af5d6bea44ac
Tareq <tareqnmd@gmail.com>
2024-03-10 13:05:41 +0600

    v2

--- dog friendly/collection_page_CTA_button_v2.js
+++ dog friendly/collection_page_CTA_button_v2.js
@@ -2,37 +2,19 @@ const product_style = `
 <style>
 	.new-save{
 		border-radius: 4px;
-		padding: 2px 8px;
-		display: flex;
-		gap: 6px;
-		align-items: center;
-		background: #99D6EA;
+		padding: 4px 8px;
 		width: max-content;
-		font-family: Poppins;
+		position: unset;
 	}
 	.new-save span{
 		font-size: 12px;
 		line-height: 12px;
+		font-family: Poppins;
 		font-weight: 700;
 		text-transform: uppercase;
 	}
 </style>
 `;
-
-const save_price_elm = (price, currency) => `
-<div class="new-save">
-	<span>Save</span>
-	<span>${currency} ${price.toFixed(2)}</span>
-</div>
-`;
-
-function numberOnly(string) {
-	return parseFloat(string.replace(',', '.').replace(/[^0-9\-+\.]/g, ''));
-}
-
-function currencyOnly(string) {
-	return string.replace(/[0-9,. ]/g, '');
-}
 
 const interval = setInterval(() => {
 	const products = [...document.querySelectorAll('.grid-product__content')];
@@ -44,6 +26,7 @@ const interval = setInterval(() => {
 			const save_child = item.querySelector(
 				'.grid-product__price_saving_wrapper'
 			);
+			save_child.classList.add('new-save');
 			price_parent.insertAdjacentElement('beforeend', save_child);
 		});
 		clearInterval(interval);
