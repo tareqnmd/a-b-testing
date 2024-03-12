094f72ab135451c9fb56c23a9ed0ee80bbc1e3ca
Tareq <tareqnmd@gmail.com>
2024-03-13 00:07:14 +0600

    pdp save

--- oz hair beauty/pdp-save.js
+++ oz hair beauty/pdp-save.js
@@ -1,3 +1,5 @@
+const PRICE_TO_SEE_OFFER = 49;
+
 const style = `
 <style>
 .new-save-elm {
@@ -6,20 +8,32 @@ const style = `
     left: 10px;
     z-index: 10;
 }
+@media only screen and (min-width:768px){
+    .new-save-elm {
+        bottom: 40px;
+    }
+}
 </style>
 `;
 
 const new_elm_html = `<img class="new-save-elm" src="https://i.ibb.co/LxnTFMj/Group.png" alt="" />`;
 
 const interval = setInterval(() => {
-	const exist_elms = [
-		...document.querySelectorAll('.relative > .Swiper_swiperCommon__TaNb5'),
-	];
+	const exist_elm = document.querySelector(
+		'.relative > .Swiper_swiperCommon__TaNb5'
+	);
+	const price_text = document.querySelector(
+		'.Price_current-price__NDbru'
+	).innerText;
+	const price_number = Number(price_text);
 	const head = document.querySelector('head');
-	if (exist_elms && head && !document.querySelector('.new-save-elm')) {
+	if (
+		exist_elm &&
+		head &&
+		price_number >= PRICE_TO_SEE_OFFER &&
+		!document.querySelector('.new-save-elm')
+	) {
 		head.insertAdjacentHTML('beforeend', style);
-		exist_elms.map((item) => {
-			item.insertAdjacentHTML('beforeend', new_elm_html);
-		});
+		exist_elm.insertAdjacentHTML('beforeend', new_elm_html);
 	}
 }, 10);
