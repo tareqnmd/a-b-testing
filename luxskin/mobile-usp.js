e272a987525247cae5ec8d8f442c5cc925cb23a1
Tareq <tareqnmd@gmail.com>
2024-03-25 00:47:10 +0600

    update

---
+++ luxskin/mobile-usp.js
@@ -0,0 +1,124 @@
+const star_svg = `
+<svg
+	width="89"
+	height="16"
+	viewBox="0 0 89 16"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect
+		width="16"
+		height="16"
+		transform="translate(0.5)"
+		fill="#FF437B"
+	/>
+	<path
+		d="M8.5 3L9.96065 5.98959L13.2553 6.45492L10.8634 8.76791L11.4389 12.0451L8.5 10.485L5.56107 12.0451L6.13662 8.76791L3.74472 6.45492L7.03935 5.98959L8.5 3Z"
+		fill="white"
+	/>
+	<rect
+		width="16"
+		height="16"
+		transform="translate(18.5)"
+		fill="#FF437B"
+	/>
+	<path
+		d="M26.5 3L27.9606 5.98959L31.2553 6.45492L28.8634 8.76791L29.4389 12.0451L26.5 10.485L23.5611 12.0451L24.1366 8.76791L21.7447 6.45492L25.0394 5.98959L26.5 3Z"
+		fill="white"
+	/>
+	<rect
+		width="16"
+		height="16"
+		transform="translate(36.5)"
+		fill="#FF437B"
+	/>
+	<path
+		d="M44.5 3L45.9606 5.98959L49.2553 6.45492L46.8634 8.76791L47.4389 12.0451L44.5 10.485L41.5611 12.0451L42.1366 8.76791L39.7447 6.45492L43.0394 5.98959L44.5 3Z"
+		fill="white"
+	/>
+	<rect
+		width="16"
+		height="16"
+		transform="translate(54.5)"
+		fill="#FF437B"
+	/>
+	<path
+		d="M62.5 3L63.9606 5.98959L67.2553 6.45492L64.8634 8.76791L65.4389 12.0451L62.5 10.485L59.5611 12.0451L60.1366 8.76791L57.7447 6.45492L61.0394 5.98959L62.5 3Z"
+		fill="white"
+	/>
+	<rect
+		width="16"
+		height="16"
+		transform="translate(72.5)"
+		fill="#FF437B"
+	/>
+	<path
+		d="M80.5 3L81.9606 5.98959L85.2553 6.45492L82.8634 8.76791L83.4389 12.0451L80.5 10.485L77.5611 12.0451L78.1366 8.76791L75.7447 6.45492L79.0394 5.98959L80.5 3Z"
+		fill="white"
+	/>
+</svg>`;
+const style = `
+<style>
+    .drawer__inner.drawer-left__inner{
+        padding-left: 0;
+        padding-right: 0;
+    }
+    .drawer__inner.drawer-left__inner ul{
+        padding: 15px 15px 0;
+    }
+    .new-rev-elm {
+        background: #F8EADD;
+        padding: 16px 0;
+        margin-top: 40px;
+        margin-bottom: 20px;
+    }
+    .new-rev-elm .rating-nav{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+    .new-rev-elm span{
+        display: flex;
+        align-items: flex-end;
+        font-size: 16px;
+        font-weight: 500;
+        color: #222239;
+    }
+    .new-rev-elm strong{
+        font-size: 20px;
+        font-weight: 800;
+    }
+    .new-rev-elm h6{
+        color: #222239;
+        font-size: 14px;
+        font-weight: 600;
+        text-align: center;
+    }
+</style>
+`;
+
+const new_elm_html = `
+<div class="new-rev-elm">
+    <div class="rating-nav">
+        <span>
+            <strong>4.9/</strong>
+            5
+        </span>
+        ${star_svg}
+    </div>
+    <h6>Over 100,000 5-star reviews!</h6>
+</div>
+`;
+
+const interval = setInterval(() => {
+	const exist_elm = document.querySelector(
+		'.drawer__inner.drawer-left__inner .mobile-nav'
+	);
+	const head = document.querySelector('head');
+	if (exist_elm && head && !document.querySelector('.new-rev-elm')) {
+		head.insertAdjacentHTML('beforeend', style);
+		exist_elm.insertAdjacentHTML('afterend', new_elm_html);
+		clearInterval(interval);
+	}
+}, 10);
---
+++ luxskin/product-layout.js
@@ -0,0 +1,54 @@
+const style = `
+<style>
+.wrapper .for_crumb {
+    display: none;
+}
+.product-single__title {
+    text-align: left;
+    margin-bottom: 0;
+}
+.product-single__meta > p {
+    display: none!important;
+}
+
+.price-area{
+    display: flex;
+    margin: 6px 0 10px;
+}
+.stamped-product-reviews-badge.stamped-main-badge .stamped-badge{
+    display: flex;
+    gap: 6px;
+    align-items: center;
+}
+#AddToCartForm--product-template{
+    margin-bottom: 16px;
+}
+body.template-product .box_quantity:hover *{
+    background: #eca1a1!important; 
+}
+body.template-product .box_quantity .qtybox{
+    border-bottom: 1px solid #e9b9b9;
+}
+@media screen and (min-width: 591px){
+    .product-single__title {
+        font-size: 1.8em!important;
+    }
+}
+</style>
+`;
+
+const interval = setInterval(() => {
+	const title = document.querySelector('.product-single__title');
+	const price = document.querySelector('div[itemprop="offers"]');
+	const rev_star = document.querySelector(
+		'.stamped-product-reviews-badge.stamped-main-badge'
+	);
+	const head = document.querySelector('head');
+	if (title && head && !document.querySelector('.price-area')) {
+		head.insertAdjacentHTML('beforeend', style);
+		title.insertAdjacentElement('beforebegin', rev_star);
+		title.insertAdjacentElement('afterend', price);
+		price.classList.add('price-area');
+		clearInterval(interval);
+	}
+}, 10);
