80ac654096138156e407aef9df46359988f2fdc4
Tareq <tareqnmd@gmail.com>
2024-04-10 17:12:43 +0600

    max width

--- missamara/wider-img.js
+++ missamara/wider-img.js
@@ -3,6 +3,11 @@ const style = `
 .product-detail .detail .title{
     text-align: left;
 }
+@media (min-width: 768px) {
+    .main-image-wrapper{
+        width: 98%!important;
+    }
+}
 </style>
 `;
 
@@ -11,10 +16,15 @@ const interval = setInterval(() => {
 		'#main-product-detail .product-price-wrapper'
 	);
 	const title = document.querySelector('#main-product-detail .title');
+	const thumbs = document.querySelector('#thumbscroll');
+	const discount_wrapper = document.querySelector(
+		'.main-image.main-image-discount-wrapper'
+	);
 	const head = document.querySelector('head');
-	if (price_wrapper && head && title) {
+	if (price_wrapper && head && discount_wrapper && thumbs && title) {
 		head.insertAdjacentHTML('beforeend', style);
 		price_wrapper.insertAdjacentElement('beforebegin', title);
+		discount_wrapper.insertAdjacentElement('afterend', thumbs);
 		clearInterval(interval);
 	}
 }, 10);
