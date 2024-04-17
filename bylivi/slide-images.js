430199266f00e5a578a014b186da98cc4e9fffe7
Tareq <tareqnmd@gmail.com>
2024-04-18 00:01:28 +0600

    slide img

---
+++ bylivi/slide-images.js
@@ -0,0 +1,40 @@
+const style = `
+<style>
+    .flickity-page-dots .dot{
+        width: 30px!important;
+        height: 30px!important;
+        overflow: hidden!important;
+        border-radius: 0!important;
+        border: 1px solid #dfdfdf!important;
+    }
+    .dot-image{
+        width: 100%;
+    }
+    // .AspectRatio:before{
+    //     display: none;
+    // }
+    .Product__SlideshowMobileNav{
+        margin: 20px 0 0!important;
+    }
+</style>
+`;
+
+const interval = setInterval(() => {
+	const dots = [...document.querySelectorAll('.flickity-page-dots button')];
+	const images = [
+		...document.querySelectorAll('.flickity-slider .AspectRatio > img'),
+	];
+	const head = document.querySelector('head');
+	if (dots && head && images && !document.querySelector('.dot-img')) {
+		try {
+			head.insertAdjacentHTML('beforeend', style);
+			dots.map((item, index) => {
+				item.innerHTML = `<img class="dot-image" src="${images[index].dataset.srcset}">`;
+			});
+		} catch (error) {
+			console.log(error);
+		} finally {
+			clearInterval(interval);
+		}
+	}
+}, 10);
