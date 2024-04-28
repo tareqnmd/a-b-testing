5ea0229632f49572ccbc08ea452117926042f5f9
Tareq <tareqnmd@gmail.com>
2024-04-29 00:56:32 +0600

    update home img

--- luxskin/home-img.js
+++ luxskin/home-img.js
@@ -1,9 +1,7 @@
 const interval = setInterval(() => {
-	const desktop_img = document.querySelector('.mobile_image img');
-	const mobile_img = document.querySelector('.desktop_image img');
-	const head = document.querySelector('head');
-	if (desktop_img && mobile_img && head) {
-		head.insertAdjacentHTML('beforeend', style);
+	const mobile_img = document.querySelector('.mobile_image img');
+	const desktop_img = document.querySelector('.desktop_image img');
+	if (desktop_img && mobile_img) {
 		desktop_img.src = 'https://i.ibb.co/ssDn4ys/Banner-desktop.png';
 		mobile_img.src = 'https://i.ibb.co/283S7rX/Banner-Mobile-2x.png';
 		clearInterval(interval);
