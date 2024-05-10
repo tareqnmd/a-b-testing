a9c40bdad1b10f0b2ffbf7d870529611298c7e13
Tareq <tareqnmd@gmail.com>
2024-05-10 11:26:54 +0600

    updated

--- carpetmart/hero.js
+++ carpetmart/hero.js
@@ -46,6 +46,27 @@ const style = `
     min-height:175px;
     object-fit: cover;
 }
+#s-f66a8593-6a71-4cd3-849c-382fc3bd04a0 .shg-btn-wrapper a{
+    background: #FF0000;
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 28.8px;
+    text-align: center;
+    border-radius: 0;
+    color: white;
+}
+#s-f66a8593-6a71-4cd3-849c-382fc3bd04a0 .shg-default-text-content p{
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 40px;
+    color: #7EC042;
+}
+#s-f66a8593-6a71-4cd3-849c-382fc3bd04a0 .shogun-heading-component h2{
+    font-size: 40px;
+    font-weight: 1000;
+    line-height: 40px;
+    color: white;
+}
 @media (max-width:1400px){
     .new-link-elm span{
         width: min-content;
@@ -105,9 +126,82 @@ const interval = setInterval(() => {
 	);
 	const box_content = exist_elm.querySelector('.shg-box-content');
 	const head = document.querySelector('head');
-	if (exist_elm && exist_elm && head && !document.querySelector('.new-elm')) {
+	const first_box = [
+		...document.querySelectorAll(
+			'#slider-s-3c5b648b-4675-427d-a0f1-fd03d77ab1e7,#slider-s-e6a46a22-2e91-4e9a-baab-fa7f1d2a35c7'
+		),
+	];
+	const second_box = [
+		...document.querySelectorAll(
+			'#s-7b2efbab-1d0d-4677-94be-c3a43d51a057,#s-1773b37f-d8a0-47b6-8224-59cd35a4767d'
+		),
+	];
+	const third_box = [
+		...document.querySelectorAll(
+			'#s-8c00b443-f71a-4ef3-b17a-af126387b6d5,#s-6df03fb6-bbd9-4b79-b41f-c4fefcf54ec6'
+		),
+	];
+	if (
+		exist_elm &&
+		second_box &&
+		first_box &&
+		third_box &&
+		head &&
+		!document.querySelector('.new-link-elms')
+	) {
 		head.insertAdjacentHTML('beforeend', style);
 		box_content.insertAdjacentHTML('afterend', new_elm_html);
+
+		try {
+			first_box.map((item) => {
+				const headings = [
+					...item.querySelectorAll('.shogun-heading-component h2'),
+				];
+				headings.map((item) => {
+					item.innerHTML = 'Carpet & Tile Mart';
+				});
+				const sub = [...item.querySelectorAll('.shg-default-text-content p')];
+				sub.map((item) => {
+					item.innerHTML =
+						'<span>Available. Affordable.</span><span>At Your Service.</span>';
+				});
+				const btns = [...item.querySelectorAll('.shg-btn-wrapper a')];
+				btns.map((item) => {
+					item.innerHTML = 'Find a Store';
+				});
+			});
+		} catch (error) {}
+
+		try {
+			second_box.map((item) => {
+				const headings = [
+					...item.querySelectorAll('.shogun-heading-component h2'),
+				];
+				headings.map((item) => {
+					item.innerHTML = 'Rugs';
+				});
+				const sub = [...item.querySelectorAll('.shg-default-text-content p')];
+				sub.map((item) => {
+					item.innerHTML = 'Up to 50% off MSRP';
+				});
+			});
+		} catch (error) {}
+
+		try {
+			third_box.map((item) => {
+				const headings = [
+					...item.querySelectorAll('.shogun-heading-component h2'),
+				];
+				headings.map((item) => {
+					item.innerHTML = 'Luxury Vinyl';
+				});
+				const sub = [...item.querySelectorAll('.shg-default-text-content p')];
+				sub.map((item) => {
+					item.innerHTML = 'The easiest waterproof flooring solution.';
+				});
+			});
+		} catch (error) {}
+
 		clearInterval(interval);
 	}
 }, 10);
