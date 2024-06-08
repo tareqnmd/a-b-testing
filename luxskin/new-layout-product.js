88689c53edf6f589edd91946e016c9f83749fcba
Tareq <tareqnmd@gmail.com>
2024-06-09 00:56:01 +0600

    new layout

---
+++ luxskin/new-layout-product.js
@@ -0,0 +1,253 @@
+const moneyBackSvg = `
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M30.375 12.375C30.3777 11.4777 30.2422 10.5851 29.9732 9.72744C29.458 8.09086 28.4782 6.63226 27.1488 5.52249C25.8193 4.41272 24.1951 3.69773 22.4665 3.46129C20.7378 3.22484 18.9763 3.47672 17.3883 4.18743C15.8003 4.89814 14.4515 6.03824 13.5 7.47419" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.375 12.2513C12.3724 13.0488 12.5052 13.8421 12.7688 14.6043C13.2737 16.0588 14.2338 17.3551 15.5367 18.3414C16.8396 19.3277 18.4313 19.9632 20.1254 20.1733C21.8194 20.3835 23.5457 20.1596 25.1019 19.528C26.6582 18.8963 27.98 17.8831 28.9125 16.6069M29.25 11.25L30.375 12.2513L31.5 11.25" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M13.5 13.5L12.375 12.375L11.25 13.5" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19.125 15.75H21.9375C22.3851 15.75 22.8143 15.5722 23.1307 15.2557C23.4472 14.9393 23.625 14.5101 23.625 14.0625C23.625 13.6149 23.4472 13.1857 23.1307 12.8693C22.8143 12.5528 22.3851 12.375 21.9375 12.375H20.8125C20.3649 12.375 19.9357 12.1972 19.6193 11.8807C19.3028 11.5643 19.125 11.1351 19.125 10.6875C19.125 10.2399 19.3028 9.81072 19.6193 9.49426C19.9357 9.17779 20.3649 9 20.8125 9H23.625M21.375 9V7.875M21.375 16.875V15.75" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.83936 25.7142L10.5531 29.6404C11.2461 30.2175 12.1195 30.5336 13.0214 30.5337H25.4459C26.3324 30.5337 27.0546 29.8137 27.0546 28.9272C27.0546 28.0744 26.7159 27.2566 26.1129 26.6536C25.5099 26.0507 24.6921 25.7119 23.8394 25.7119H15.2005" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.6252 23.7861L13.072 25.2329C13.262 25.4228 13.4875 25.5734 13.7357 25.6762C13.9839 25.7789 14.2499 25.8318 14.5186 25.8317C14.7872 25.8317 15.0532 25.7787 15.3014 25.6759C15.5495 25.573 15.775 25.4223 15.9649 25.2323C16.1548 25.0423 16.3055 24.8168 16.4082 24.5686C16.511 24.3204 16.5638 24.0543 16.5638 23.7857C16.5637 23.5171 16.5108 23.2511 16.4079 23.0029C16.3051 22.7548 16.1543 22.5293 15.9644 22.3394L13.72 20.0927C13.3612 19.7343 12.9353 19.4502 12.4666 19.2566C11.9979 19.063 11.4956 18.9637 10.9885 18.9644H5.83936" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+const lowPriceSvg = `
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22.4663 18H14.5337V25.8143H11L18.5 32.625L26 25.8143H22.4663V18Z" stroke="black"/>
+<path d="M18.5625 22.5C23.8437 22.5 28.125 18.4706 28.125 13.5C28.125 8.52944 23.8437 4.5 18.5625 4.5C13.2813 4.5 9 8.52944 9 13.5C9 18.4706 13.2813 22.5 18.5625 22.5Z" fill="white" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.4531 18H19.6172C20.0834 18 20.5305 17.7926 20.8601 17.4234C21.1898 17.0542 21.375 16.5534 21.375 16.0312C21.375 15.5091 21.1898 15.0083 20.8601 14.6391C20.5305 14.2699 20.0834 14.0625 19.6172 14.0625H17.5078C17.0416 14.0625 16.5945 13.8551 16.2649 13.4859C15.9352 13.1167 15.75 12.6159 15.75 12.0938C15.75 11.5716 15.9352 11.0708 16.2649 10.7016C16.5945 10.3324 17.0416 10.125 17.5078 10.125H20.6719" fill="white"/>
+<path d="M16.4531 18H19.6172C20.0834 18 20.5305 17.7926 20.8601 17.4234C21.1898 17.0542 21.375 16.5534 21.375 16.0313C21.375 15.5091 21.1898 15.0083 20.8601 14.6391C20.5305 14.2699 20.0834 14.0625 19.6172 14.0625H17.5078C17.0416 14.0625 16.5945 13.8551 16.2649 13.4859C15.9352 13.1167 15.75 12.6159 15.75 12.0938C15.75 11.5716 15.9352 11.0708 16.2649 10.7016C16.5945 10.3324 17.0416 10.125 17.5078 10.125H20.6719" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19.125 18V19.125" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19.125 7.875V10.125" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+const returnSvg = `
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.542 14.6606L18.0014 18.4022L24.412 14.685M18.0026 25.0322V18.39" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.9678 15.9281C10.9678 14.9166 11.6869 13.6856 12.5766 13.1981L16.4766 11.0288C17.3053 10.5656 18.6703 10.5656 19.5113 11.0288L23.4113 13.1981C24.2888 13.6856 25.02 14.9166 25.02 15.9281V20.0597C25.02 21.0713 24.3009 22.3022 23.4113 22.7897L19.5113 24.9591C18.6825 25.4222 17.3175 25.4222 16.4766 24.9591L12.5766 22.7897C12.1378 22.5459 11.7356 22.1194 11.4431 21.6197M30.1875 21.6563C30.1875 26.3728 26.3728 30.1875 21.6563 30.1875L22.9359 28.0547M5.8125 14.3438C5.8125 9.62719 9.62719 5.8125 14.3438 5.8125L13.0641 7.94531" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+const truckSvg = `
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M26.25 22.5V9C26.25 8.60218 26.092 8.22064 25.8107 7.93934C25.5294 7.65804 25.1478 7.5 24.75 7.5H8.25M6.75 24C6.75 24.3978 6.90804 24.7794 7.18934 25.0607C7.47064 25.342 7.85218 25.5 8.25 25.5H11.25M23.25 25.5H17.25M29.25 25.5H32.25C32.6478 25.5 33.0294 25.342 33.3107 25.0607C33.592 24.7794 33.75 24.3978 33.75 24V18L31.17 12.825C31.0442 12.5753 30.8512 12.3658 30.6126 12.22C30.3741 12.0743 30.0995 11.9981 29.82 12H26.25M11.25 12.22H2.8125M9.5625 15.1877H3.375M12.375 18H6.1875" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.25 25.5C17.25 26.2956 16.9339 27.0587 16.3713 27.6213C15.8087 28.1839 15.0456 28.5 14.25 28.5C13.4544 28.5 12.6913 28.1839 12.1287 27.6213C11.5661 27.0587 11.25 26.2956 11.25 25.5C11.25 24.7043 11.5661 23.9413 12.1287 23.3787C12.6913 22.8161 13.4544 22.5 14.25 22.5C15.0456 22.5 15.8087 22.8161 16.3713 23.3787C16.9339 23.9413 17.25 24.7043 17.25 25.5ZM26.25 22.5C25.4544 22.5 24.6913 22.8161 24.1287 23.3787C23.5661 23.9413 23.25 24.7043 23.25 25.5C23.25 26.2956 23.5661 27.0587 24.1287 27.6213C24.6913 28.1839 25.4544 28.5 26.25 28.5C27.0456 28.5 27.8087 28.1839 28.3713 27.6213C28.9339 27.0587 29.25 26.2956 29.25 25.5C29.25 24.7043 28.9339 23.9413 28.3713 23.3787C27.8087 22.8161 27.0456 22.5 26.25 22.5Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+const style = `
+<style>
+.new-points-elm{
+    padding: 20px;
+    gap: 10px;
+    border-radius: 8px;
+    background: linear-gradient(90deg, #F9EADC 0%, #F5EDE1 100%);
+    display: grid;
+    place-items: flex-start;
+    margin-bottom: 20px;
+}
+.new-points-elm *{
+    margin: 0;
+    padding: 0;
+    letter-spacing: 0px;
+    text-align: left;
+}
+.new-points-elm h6{
+    color: #0D0C22;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 14px;
+    text-transform: capitalize;
+}
+.new-points-elm h6 span{
+    font-weight: 700;
+}
+.new-points-elm strong{
+    color: #0D0C22;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 16px;
+}
+.new-points-elm ul li{
+    color: #0D0C22;
+    margin-left: 20px;
+    font-size: 14px;
+    font-weight: 400;
+    text-align: left;
+}
+ul.key-point{
+    display: none!important;
+}
+.new-usps{
+    display: flex;
+    align-items: center;
+    gap:2px;
+    justify-content: space-between;
+    margin-bottom: 20px;
+}
+.new-usps .single-usp{
+    display: flex;
+    align-items: center;
+    gap:2px;
+}
+.new-usps .single-usp span{
+    font-size: 10px;
+    font-weight: 500;
+    color: #0D0C22;
+    text-align: left!important;
+    line-height: 14px;
+}
+.new-usps .divider{
+    width: 1px;
+    height: 20px;
+    background: black;
+}
+.product-single__meta--wrapper{
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+.box_quantity{
+    gap: 20px;
+}
+
+#AddToCart--product-template{
+    gap: 10px;
+    border-radius: 8px;
+    background: linear-gradient(90deg, #FF437B 0%, #EA2661 100%)!important;
+    color: white!important;
+}
+
+.box_quantity .qtydiv{
+    background: none!important;
+}
+
+.qtybox {
+    background: #F5F5F5!important;
+    border: 1px solid #E9E9E9!important;
+    overflow: hidden;
+    border-radius: 8px;
+}
+
+#ProductSelect ~ .box_quantity:hover *{
+    background: #F5F5F5!important;
+}
+
+#ProductSelect ~ .box_quantity:hover input{
+    background: #ffffff!important;
+}
+
+#ProductSelect ~ .box_quantity:hover button{
+    background: linear-gradient(90deg, #FF437B 0%, #EA2661 100%)!important;
+}
+
+.qtybox input{
+    background: #ffffff!important;
+    border:0!important;
+}
+
+.top-message{
+    color: #C39576;
+    gap: 10px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+
+.top-message span{
+    border: 1px solid #C39576;  
+    padding: 8px 16px;
+    border-radius: 8px;
+}
+
+.sub-title{
+    text-align: left!important;
+    margin-bottom: 10px;
+    display: block;
+}
+
+@media screen and (max-width: 590px){
+    .product-single__meta--wrapper{
+        margin: 0;
+        padding: 0;
+    }
+}
+
+@media screen and (max-width: 480px){
+    .new-usps .single-usp span{
+        font-size: 8px;
+    }
+}
+</style>
+`;
+
+const new_elm_html = `
+<div class="new-points-elm">
+    <h6><span>What it is:</span> Most affordable and effective LED Mask on the market</h6>
+    <strong>Why it’s special:</strong>
+    <ul>
+        <li>Clinically proven benefits</li>
+        <li>Reduce fine lines and wrinkles</li>
+        <li>Promote glowing, younger looking skin</li>
+        <li>Reduce skin bacteria and acne</li>
+    </ul>
+</div>
+`;
+
+const new_usps = `
+<div class="new-usps">
+    <div class="single-usp">
+        ${truckSvg}
+        <span>Free</br>shipping</span>
+    </div>
+    <div class="single-usp">
+        ${returnSvg}
+        <span>Easy</br>returns</span>
+    </div>
+    <div class="single-usp">
+        ${lowPriceSvg}
+        <span>50-80%</br>LOWER Price</span>
+    </div>
+    <div class="single-usp">
+        ${moneyBackSvg}
+        <span>Money</br>back guarantee</span>
+    </div>
+</div>
+`;
+
+const newMsg = `
+<div class='top-message'>
+    <span>Smooth</span>
+    <span>Refreshing</span>
+    <span>Vegan</span>
+</div>
+`;
+
+const interval = setInterval(() => {
+	try {
+		const exist_elm = document.querySelector('.key-point');
+		const form_elm = document.querySelector('#AddToCartForm--product-template');
+		const add_cart = document.querySelector('#AddToCart--product-template');
+		const product_price = document.querySelector('#ProductPrice');
+		const single_img = document.querySelector(
+			'.product-single__photo-wrapper img'
+		);
+		const title = document.querySelector('.product-single__title');
+		const rev = document.querySelector(
+			'.stamped-product-reviews-badge.stamped-main-badge'
+		);
+		const head = document.querySelector('head');
+		if (exist_elm && head && !document.querySelector('.new-points-elm')) {
+			head.insertAdjacentHTML('beforeend', style);
+			title.insertAdjacentElement('afterend', rev);
+			title.insertAdjacentHTML(
+				'afterend',
+				'<strong class="sub-title">Covers all Body parts</strong>'
+			);
+			exist_elm.previousElementSibling.style.display = 'none';
+			exist_elm.insertAdjacentHTML('beforebegin', new_elm_html);
+			form_elm.insertAdjacentHTML('afterend', new_usps);
+			single_img.src = '';
+			add_cart.innerHTML = `ADD TO CART | ${product_price.innerText}`;
+			clearInterval(interval);
+		}
+	} catch (error) {
+		console.log('product-usp', error);
+	}
+}, 10);
