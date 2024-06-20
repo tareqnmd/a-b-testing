9bd415cf1f681fb6b015c83935911346a627af85
Tareq <tareqnmd@gmail.com>
2024-06-20 17:14:47 +0600

    v7 updated

---
+++ missamara/product-accordion-v7.js
@@ -0,0 +1,271 @@
+const arrowUpSvg = `
+<svg class="content-open" width="17" height="11" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.875 10L8.50054 2L1.12609 10" stroke="#333333" stroke-width="2"/>
+</svg>
+`;
+const arrowDownSvg = `
+<svg class="content-close" width="17" height="11" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.12891 1L8.50336 9L15.8778 1" stroke="#333333" stroke-width="2"/>
+</svg>
+`;
+const washingMachineSvg = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.6148 2H5.17484C4.64484 2 4.21484 2.448 4.21484 3V21C4.21484 21.552 4.64484 22 5.17484 22H18.6148C19.1448 22 19.5748 21.552 19.5748 21V3C19.5748 2.448 19.1448 2 18.6148 2Z" stroke="#333333" stroke-width="2"/>
+<path d="M4.21484 7.75H19.5748" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M13.8116 6C14.3416 6 14.7716 5.552 14.7716 5C14.7716 4.448 14.3416 4 13.8116 4C13.2816 4 12.8516 4.448 12.8516 5C12.8516 5.552 13.2816 6 13.8116 6ZM16.6916 6C17.2216 6 17.6516 5.552 17.6516 5C17.6516 4.448 17.2216 4 16.6916 4C16.1616 4 15.7316 4.448 15.7316 5C15.7316 5.552 16.1616 6 16.6916 6Z" fill="#333333"/>
+<path d="M11.8912 18.5C13.7472 18.5 15.2513 16.933 15.2513 15C15.2513 13.067 13.7472 11.5 11.8912 11.5C10.0362 11.5 8.53125 13.067 8.53125 15C8.53125 16.933 10.0362 18.5 11.8912 18.5Z" stroke="#333333" stroke-width="2"/>
+</svg>
+`;
+const descriptionSvg = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21 2H3C2.73478 2 2.48043 2.10536 2.29289 2.29289C2.10536 2.48043 2 2.73478 2 3V21C2 21.2652 2.10536 21.5196 2.29289 21.7071C2.48043 21.8946 2.73478 22 3 22H21C21.2652 22 21.5196 21.8946 21.7071 21.7071C21.8946 21.5196 22 21.2652 22 21V3C22 2.73478 21.8946 2.48043 21.7071 2.29289C21.5196 2.10536 21.2652 2 21 2ZM4 4H20V6H4V4ZM20 20H4V8H20V20ZM6 12C6 11.7348 6.10536 11.4804 6.29289 11.2929C6.48043 11.1054 6.73478 11 7 11H17C17.2652 11 17.5196 11.1054 17.7071 11.2929C17.8946 11.4804 18 11.7348 18 12C18 12.2652 17.8946 12.5196 17.7071 12.7071C17.5196 12.8946 17.2652 13 17 13H7C6.73478 13 6.48043 12.8946 6.29289 12.7071C6.10536 12.5196 6 12.2652 6 12ZM6 16C6 15.7348 6.10536 15.4804 6.29289 15.2929C6.48043 15.1054 6.73478 15 7 15H12C12.2652 15 12.5196 15.1054 12.7071 15.2929C12.8946 15.4804 13 15.7348 13 16C13 16.2652 12.8946 16.5196 12.7071 16.7071C12.5196 16.8946 12.2652 17 12 17H7C6.73478 17 6.48043 16.8946 6.29289 16.7071C6.10536 16.5196 6 16.2652 6 16Z" fill="#333333"/>
+</svg>
+`;
+const featuresSvg = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 10H4C3.46957 10 2.96086 9.78929 2.58579 9.41421C2.21071 9.03914 2 8.53043 2 8V4C2 3.46957 2.21071 2.96086 2.58579 2.58579C2.96086 2.21071 3.46957 2 4 2H20C20.5304 2 21.0391 2.21071 21.4142 2.58579C21.7893 2.96086 22 3.46957 22 4V8C22 8.53043 21.7893 9.03914 21.4142 9.41421C21.0391 9.78929 20.5304 10 20 10H19M5 14H4C3.46957 14 2.96086 14.2107 2.58579 14.5858C2.21071 14.9609 2 15.4696 2 16V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H20C20.5304 22 21.0391 21.7893 21.4142 21.4142C21.7893 21.0391 22 20.5304 22 20V16C22 15.4696 21.7893 14.9609 21.4142 14.5858C21.0391 14.2107 20.5304 14 20 14H19M6 6H6.01M6 18H6.01" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12 8V9M12 15V16M16 12H15M9 12H8M15 9L14.12 9.88M9.88 14.12L9 15M15 15L14.12 14.12M9.88 9.88L9 9" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+const questSvg = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 1C18.0753 1 23 5.9247 23 12C23 18.0753 18.0753 23 12 23C5.9247 23 1 18.0753 1 12C1 5.9247 5.9247 1 12 1ZM12 3.2C9.66609 3.2 7.42778 4.12714 5.77746 5.77746C4.12714 7.42778 3.2 9.66609 3.2 12C3.2 14.3339 4.12714 16.5722 5.77746 18.2225C7.42778 19.8729 9.66609 20.8 12 20.8C14.3339 20.8 16.5722 19.8729 18.2225 18.2225C19.8729 16.5722 20.8 14.3339 20.8 12C20.8 9.66609 19.8729 7.42778 18.2225 5.77746C16.5722 4.12714 14.3339 3.2 12 3.2ZM12 16.4C12.2917 16.4 12.5715 16.5159 12.7778 16.7222C12.9841 16.9285 13.1 17.2083 13.1 17.5C13.1 17.7917 12.9841 18.0715 12.7778 18.2778C12.5715 18.4841 12.2917 18.6 12 18.6C11.7083 18.6 11.4285 18.4841 11.2222 18.2778C11.0159 18.0715 10.9 17.7917 10.9 17.5C10.9 17.2083 11.0159 16.9285 11.2222 16.7222C11.4285 16.5159 11.7083 16.4 12 16.4ZM12 5.95C12.9265 5.95003 13.8241 6.27269 14.5386 6.86256C15.253 7.45243 15.7398 8.27269 15.9152 9.18244C16.0907 10.0922 15.9438 11.0346 15.5 11.8479C15.0561 12.6611 14.3429 13.2944 13.4828 13.639C13.3561 13.6871 13.2415 13.7626 13.1473 13.8601C13.0989 13.9151 13.0912 13.9855 13.0923 14.0581L13.1 14.2C13.0997 14.4804 12.9923 14.75 12.7999 14.9539C12.6074 15.1578 12.3444 15.2805 12.0645 15.2969C11.7846 15.3133 11.509 15.2223 11.294 15.0423C11.079 14.8624 10.9408 14.6071 10.9077 14.3287L10.9 14.2V13.925C10.9 12.6567 11.923 11.8955 12.6644 11.5974C12.9661 11.4769 13.2294 11.2765 13.4258 11.0177C13.6222 10.7589 13.7445 10.4514 13.7794 10.1284C13.8142 9.80538 13.7605 9.47894 13.6238 9.18415C13.4872 8.88937 13.2728 8.63737 13.0038 8.45522C12.7347 8.27308 12.4211 8.16766 12.0967 8.1503C11.7722 8.13293 11.4492 8.20427 11.1622 8.35666C10.8753 8.50905 10.6352 8.73672 10.4679 9.01524C10.3006 9.29375 10.2123 9.61259 10.2125 9.9375C10.2125 10.2292 10.0966 10.509 9.89032 10.7153C9.68403 10.9216 9.40424 11.0375 9.1125 11.0375C8.82076 11.0375 8.54097 10.9216 8.33468 10.7153C8.12839 10.509 8.0125 10.2292 8.0125 9.9375C8.0125 8.87995 8.43261 7.86571 9.18041 7.11791C9.92821 6.37011 10.9424 5.95 12 5.95Z" fill="#333333"/>
+</svg>
+`;
+
+const style = `
+<style>
+    #wmg-instruction-container, .detail.layout-column-half-right .product-details-background-wrapper:not(:first-child){
+        display: none!important;
+    }
+    .new-accordion-elm .accordion {
+        border: 1px solid #C9C9C9;
+        border-bottom: 0;
+        display: none;
+    }
+    .accordion.content-added{
+        display: block;
+    }
+    .accordion .header{
+        background: #ffffff;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 20px;
+        cursor: pointer;
+        color: #333333;
+        font-family:Lato;
+    }
+    .accordion.active .header{
+        background: #fff3e2;
+    }
+    .accordion.active .header{
+        color: #d88a1c;
+    }
+    .accordion.active .header *{
+        stroke: #d88a1c;
+    }
+    .accordion .header strong{
+        font-size: 20px;
+        font-weight: 700;
+        flex-grow: 1;
+        font-family: Lato;
+        line-height: 24px;
+    }
+    .accordion .header svg{
+        fill: #ffffff00;
+        flex-shrink: 0;
+    }
+    .accordion .header img{
+        max-width: 24px;
+    }
+    .accordion.active .header img{
+        filter: invert(53%) sepia(70%) saturate(2128%) hue-rotate(8deg) brightness(103%) contrast(78%);
+    }
+    .accordion .content{
+        display: none;
+        padding:20px;
+    }
+    .accordion .header svg.content-close{
+        display: none;
+    }
+    .accordion.active .header svg.content-open{
+        display: none;
+    }
+    .accordion.active .header svg.content-close{
+        display: block;
+    }
+    .accordion.active .content{
+        display: block;
+    }
+    .accordion.active .content .wmg-instruction-wrapper{
+        padding: 0!important;
+    }
+    svg.content-open, svg.content-close{
+        width: 14px!important;
+        height: 14px!important;
+    }
+    .accordion .wmg-header, .accordion .wmg-check-wrapper, #pdp-education-accordian-container .pdp-education-container,
+    #shopify-section-pdp-education {
+        display: none!important;
+    }
+    .accordion.content-added:last-child{
+        border-bottom: 1px solid #C9C9C9;
+    }
+    .accordion.content-added .badge-accordian-description{
+        padding:0;
+    }    
+</style>
+`;
+
+const new_elm_html = (existingContent) => `
+<div class="new-accordion-elm container">
+    <div class="accordion">
+        <div class="header">
+            ${washingMachineSvg}
+            <strong>Washing Machine Capacity</strong>
+            ${arrowUpSvg}
+            ${arrowDownSvg}
+        </div>
+        <div class="content"></div>
+    </div>
+    <div class="accordion">
+        <div class="header">
+            ${descriptionSvg}
+            <strong>Description</strong>
+            ${arrowUpSvg}
+            ${arrowDownSvg}
+        </div>
+        <div class="content"></div>
+    </div>
+    <div class="accordion">
+        <div class="header">
+            ${featuresSvg}
+            <strong>Features</strong>
+            ${arrowUpSvg}
+            ${arrowDownSvg}
+        </div>
+        <div class="content"></div>
+    </div>
+    <div class="accordion">
+        <div class="header">
+            ${questSvg}
+            <strong>How to check your washing machine load capacity?</strong>
+            ${arrowUpSvg}
+            ${arrowDownSvg}
+        </div>
+        <div class="content"></div>
+    </div>
+    ${existingContent}
+</div>
+`;
+
+const addElm = (img, title, content) => `
+<div class="accordion">
+    <div class="header existing">
+        <img src=${img} alt="" />
+        <strong>${title}</strong>
+        ${arrowUpSvg}
+        ${arrowDownSvg}
+    </div>
+    <div class="content">
+        ${content}
+    </div>
+</div>
+`;
+
+const mainInterval = setInterval(() => {
+	try {
+		const mainProduct = document.querySelector('#main-product-detail');
+		const pdpEducation =
+			document.querySelectorAll(
+				'#shopify-section-pdp-education .accordion-item'
+			) ?? null;
+		const head = document.querySelector('head');
+		if (mainProduct && head && !document.querySelector('.new-accordion-elm')) {
+			head.insertAdjacentHTML('beforeend', style);
+			let existingContent = '';
+			if (pdpEducation) {
+				pdpEducation.forEach((item) => {
+					const headerImg = item.querySelector(
+						'.accordion-header-content img'
+					).src;
+					const headerTitle = item.querySelector(
+						'.accordion-header-content p'
+					).innerText;
+					const accordionBody = item.querySelector('.accordion-body').innerHTML;
+					existingContent += addElm(headerImg, headerTitle, accordionBody);
+				});
+			}
+			mainProduct.insertAdjacentHTML('afterend', new_elm_html(existingContent));
+			clearInterval(mainInterval);
+		}
+	} catch (error) {
+		console.log('error', error);
+	}
+}, 10);
+
+const accInterval = setInterval(() => {
+	try {
+		const accordions = document.querySelectorAll('.accordion .header') ?? null;
+		const machineLoad =
+			document.querySelector('.wmg-instruction-wrapper') ?? null;
+		const productTabsOne =
+			document.querySelector('#productTabs #tab-1>div') ?? null;
+		const productTabsTwo =
+			document.querySelector('#productTabs #tab-2>div') ?? null;
+		const machineWashable = document.querySelector('.wmg-wrapper') ?? null;
+
+		let active = 0;
+		if (accordions && !document.querySelector('.content-added')) {
+			accordions.forEach((item, index) => {
+				const parentItem = item.parentNode;
+				if (!parentItem.querySelector('.existing')) {
+					if (index === 0 && machineWashable) {
+						parentItem.classList.add('content-added');
+						active === 0 ? parentItem.classList.add('active') : null;
+						active = 1;
+						parentItem
+							.querySelector('.content')
+							.insertAdjacentElement('afterbegin', machineWashable);
+					}
+					if (index === 1 && productTabsOne) {
+						parentItem.classList.add('content-added');
+						active === 0 ? parentItem.classList.add('active') : null;
+						active = 1;
+						parentItem
+							.querySelector('.content')
+							.insertAdjacentElement('afterbegin', productTabsOne);
+					}
+					if (index === 2 && productTabsTwo) {
+						parentItem.classList.add('content-added');
+						active === 0 ? parentItem.classList.add('active') : null;
+						active = 1;
+						parentItem
+							.querySelector('.content')
+							.insertAdjacentElement('afterbegin', productTabsTwo);
+					}
+					if (index === 3 && machineLoad) {
+						parentItem.classList.add('content-added');
+						active === 0 ? parentItem.classList.add('active') : null;
+						active = 1;
+						parentItem
+							.querySelector('.content')
+							.insertAdjacentElement('afterbegin', machineLoad);
+					}
+				} else {
+					parentItem.classList.add('content-added');
+				}
+				item.addEventListener('click', () => {
+					accordions.forEach((item) => {
+						item.parentNode.classList.remove('active');
+					});
+					parentItem.classList.add('active');
+				});
+			});
+			clearInterval(accInterval);
+		}
+	} catch (error) {
+		console.log('error', error);
+	}
+}, 10);
