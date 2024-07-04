e5bcc0efd5ccc62b7ad586e082768f3aa9c4c60a
Tareq <tareqnmd@gmail.com>
2024-07-05 00:28:54 +0600

    update

---
+++ oz hair beauty/checkout-pdp.js
@@ -0,0 +1,133 @@
+const freeReturnSvg = `
+<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4531_699)">
+<path d="M9.33398 27.9827V14.6246L20.0063 8L30.6673 14.6246V27.9827L20.0063 34.6667L9.33398 27.9827Z" fill="#E6CEFF"/>
+<path d="M11.8349 6.78093C16 3.3337 22.6667 2.66686 27.3334 5.3337C31.6939 7.82571 34 11.9999 34 16.0004" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.0491 9.04756L11.8348 6.78176L13.7177 2.38255" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.334 37.3334L28.0007 31.1059V18.6667L17.334 24.8942V37.3334Z" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.3333 37.3334L8 31.1048V18.6667L17.3333 24.8942V37.3334Z" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M28 18.6351L18.0026 12L8 18.6351L18.0026 25.3333L28 18.6351Z" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_4531_699">
+<rect width="40" height="40" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+const freeShippingSvg = `
+<svg width="40" height="28" viewBox="0 0 40 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4531_710)">
+<path d="M38.9399 18.1955C38.9219 17.6824 38.7933 17.179 38.5627 16.7195C37.4507 14.659 36.2802 12.6248 35.1418 10.5765C35.0852 10.4612 34.9941 10.3661 34.881 10.3043C34.7679 10.2424 34.6383 10.2168 34.5099 10.2309C33.2206 10.2412 31.9313 10.2309 30.6429 10.2309H29.9365V4.5H8.20508V7.16081L8.21357 17.2561V21.5503C8.3192 21.5578 8.43804 21.5719 8.55594 21.5728C9.982 21.5728 13.168 21.5681 14.5922 21.5794C14.5345 21.7845 14.5056 21.9967 14.5064 22.2097C14.5069 22.5322 14.5771 22.8508 14.7122 23.1441C14.8474 23.4373 15.0443 23.6984 15.2897 23.9096C15.5352 24.1208 15.8234 24.2773 16.1349 24.3684C16.4465 24.4596 16.774 24.4833 17.0956 24.438C17.4171 24.3926 17.7252 24.2793 17.9988 24.1057C18.2725 23.9321 18.5055 23.7021 18.682 23.4314C18.8586 23.1607 18.9746 22.8556 19.0222 22.5366C19.0698 22.2175 19.048 21.8921 18.9581 21.5822C21.9611 21.5691 26.6496 21.5737 29.6517 21.5728C29.7318 21.5728 29.813 21.5606 29.9346 21.5513V21.5766C30.1034 21.5766 32.0605 21.5616 32.1982 21.5766H32.2142C32.1184 21.8871 32.091 22.2146 32.1341 22.5366C32.1771 22.8586 32.2895 23.1676 32.4637 23.4426C32.6378 23.7176 32.8696 23.9521 33.1432 24.1302C33.4168 24.3082 33.7259 24.4256 34.0493 24.4744C34.3727 24.5231 34.7029 24.5021 35.0174 24.4128C35.3319 24.3234 35.6233 24.1678 35.8718 23.9565C36.1203 23.7453 36.3201 23.4833 36.4574 23.1885C36.5948 22.8937 36.6666 22.5729 36.6679 22.2481C36.668 22.02 36.6363 21.7931 36.5735 21.5737C36.7622 21.555 38.7306 21.5737 38.9616 21.5737C38.9616 20.4208 38.9994 19.3054 38.9447 18.1955" fill="#E6CEFF"/>
+<path d="M34.4683 23.5C34.6601 23.4803 36.6739 23.5 36.9089 23.5C36.9089 22.2802 36.9482 21.1018 36.8916 19.9273C36.874 19.3846 36.7431 18.852 36.508 18.3657C35.3717 16.1842 34.1758 14.0363 33.0117 11.8696C32.9539 11.7476 32.8607 11.6469 32.745 11.5815C32.6293 11.516 32.4966 11.489 32.3654 11.5041C31.0478 11.5149 27.6914 11.5041 27.6914 11.5041V23.5H30.0006" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.8481 23.5L27.6923 23.4948V4.5H8.20508V7.45991" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.20508 18.5V23.4666L11.282 23.5" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.20438 18.5L3.07617 18.5" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6.15385 8.5H0" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.2557 11.5H3.07617" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.73258 15.4756H1.58984" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.793 24.2398C10.7948 24.7263 10.94 25.2014 11.2101 25.6048C11.4802 26.0083 11.8632 26.322 12.3106 26.5064C12.758 26.6908 13.2496 26.7374 13.7234 26.6405C14.1971 26.5436 14.6316 26.3075 14.972 25.962C15.3123 25.6165 15.5432 25.1771 15.6354 24.6996C15.7276 24.222 15.6769 23.7276 15.4898 23.279C15.3028 22.8304 14.9877 22.4477 14.5844 22.1794C14.1812 21.911 13.7079 21.7691 13.2245 21.7715C12.9037 21.7726 12.5863 21.8373 12.2904 21.962C11.9945 22.0867 11.7259 22.2689 11.5 22.4981C11.2742 22.7274 11.0955 22.9992 10.9742 23.2981C10.8528 23.597 10.7913 23.917 10.793 24.2398Z" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M34.6431 24.2858C34.6431 23.7997 34.4999 23.3246 34.2317 22.9203C33.9635 22.5161 33.5823 22.2009 33.1362 22.0145C32.6901 21.8282 32.1991 21.7791 31.7253 21.8734C31.2515 21.9677 30.8161 22.2012 30.4741 22.5444C30.132 22.8876 29.8988 23.3251 29.8037 23.8017C29.7086 24.2783 29.756 24.7725 29.9398 25.222C30.1237 25.6714 30.4358 26.056 30.8367 26.3271C31.2377 26.5982 31.7094 26.7436 32.1924 26.745C32.5143 26.7474 32.8335 26.6855 33.1315 26.5629C33.4294 26.4402 33.7002 26.2593 33.9281 26.0306C34.1561 25.8018 34.3367 25.5298 34.4594 25.2303C34.5821 24.9308 34.6446 24.6098 34.6431 24.2858Z" stroke="#3D3D4E" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_4531_710">
+<rect width="40" height="28" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+const reviewSvg = `
+<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_4531_725)">
+<path d="M22.5758 0L26.7564 13.3261H40.2851L29.3401 21.5621L33.5207 34.8882L22.5758 26.6522L11.6308 34.8882L15.8114 21.5621L4.86644 13.3261H18.3952L22.5758 0Z" fill="#E6CEFF"/>
+<path d="M18.6207 3.09885L22.3242 14.9042L22.4341 15.2546H22.8013H34.834L25.0844 22.591L24.8023 22.8033L24.908 23.1402L28.6227 34.9814L18.9213 27.6811L18.6207 27.4549L18.3201 27.6811L8.61865 34.9814L12.3334 23.1402L12.4391 22.8033L12.157 22.591L2.40739 15.2546H14.4401H14.8073L14.9172 14.9042L18.6207 3.09885Z" stroke="#3D3D4E"/>
+</g>
+<defs>
+<clipPath id="clip0_4531_725">
+<rect width="40" height="40" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+const pdps = [
+	{
+		icon: freeReturnSvg,
+		title: 'FREE RETURNS',
+		desc: '140 day returns',
+	},
+	{
+		icon: freeShippingSvg,
+		title: 'FREE & FAST DELIVERY.',
+		desc: 'Free Shipping Over $49',
+	},
+	{
+		icon: reviewSvg,
+		title: '90k+ Reviews',
+		desc: '95% 5-star reviews',
+	},
+];
+
+const style = `
+<style>
+    .checkout-pdps {
+        margin: 30px 0;
+        display: grid;
+        gap: 24px;
+    }
+    .checkout-pdp {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        gap: 4px 12px;
+        background: #FAF5FF;
+        border: 1px solid #CFC2DD;
+        border-radius: 4px;
+        padding: 16px 20px;
+    }
+    .checkout-pdp-icon {
+       flex-shrink: 0;
+       grid-row : span 2;
+    }
+    .checkout-pdp-title {
+       font-size: 14px;
+       font-weight: 600;
+       color: #3D3D4E;
+    }
+    .checkout-pdp-desc {
+       color: #3D3D4E;
+       font-size: 12px;
+       font-weight: 300;
+    }
+</style>
+`;
+
+const revsElm = `
+<div class="checkout-pdps">
+   ${pdps
+			.map(
+				(item) => `
+    <div class="checkout-pdp">
+        <div class="checkout-pdp-icon">${item.icon}</div>
+        <div class="checkout-pdp-title">${item.title}</div>
+        <div class="checkout-pdp-desc">${item.desc}</div>
+    </div>
+    `
+			)
+			.join('')}
+</div>
+`;
+
+const interval = setInterval(() => {
+	try {
+		const orderSummary = document.querySelector('#order-summary');
+		const head = document.querySelector('head');
+		if (orderSummary && head && !document.querySelector('.checkout-pdps')) {
+			head.insertAdjacentHTML('beforeend', style);
+			orderSummary.insertAdjacentHTML('beforeend', revsElm);
+		}
+	} catch (error) {
+		console.log('error', error);
+	}
+}, 10);
