f20eb56d890eb9d19515ddabd79162559725ba81
Tareq <tareqnmd@gmail.com>
2024-07-15 18:15:24 +0600

    update

--- the vortex/product-float-cart.js
+++ the vortex/product-float-cart.js
@@ -17,6 +17,7 @@ const style = `
     	flex-direction: column;
 		gap: 4px;
 		box-shadow: 0px -3px 2px #dcdcdc40;
+		z-index: 1;
 	}
 	.float-cart-btn button{
 		position: static!important;
@@ -28,17 +29,30 @@ const style = `
 </style>
 `;
 
+const getCurrentPrice = () => {
+	try {
+		const radioPriceName = document.querySelector('.kaching-bundles__bars .kaching-bundles__bar input').name;
+		const selectedPrice = document.querySelector(`input[name="${radioPriceName}"]:checked`);
+		const currentPrice = selectedPrice.parentNode.querySelector('.kaching-bundles__bar-price').innerText;
+		return currentPrice;
+	} catch (error) {
+		return false;
+	}
+};
+
 const interval = setInterval(() => {
 	try {
 		const productFloatBtn = document.querySelector('.sc-bLdqUH.eCxunz.pf-3010_.pf-button-1');
 		const productRev = document.querySelector('.sc-fHeRUh.ihgygX.pf-2619_');
+		const currentPrice = getCurrentPrice();
 		const head = document.querySelector('head');
-		if (productFloatBtn && productRev && head && !document.querySelector('.float-cart-btn')) {
+		if (currentPrice && productFloatBtn && productRev && head && !document.querySelector('.float-cart-btn')) {
 			const floatCartBtnParent = productFloatBtn.parentNode;
 			if (floatCartBtnParent) {
 				head.insertAdjacentHTML('beforeend', style);
 				const newDiv = document.createElement('div');
 				newDiv.classList.add('float-cart-btn');
+				productFloatBtn.innerHTML = `${currentPrice} - Add To Cart`;
 				newDiv.insertAdjacentElement('afterbegin', productFloatBtn);
 				newDiv.insertAdjacentHTML('afterbegin', productRev.outerHTML);
 				floatCartBtnParent.insertAdjacentElement('beforeend', newDiv);
@@ -47,5 +61,16 @@ const interval = setInterval(() => {
 		}
 	} catch (error) {
 		console.log('error', error);
+	}
+}, 10);
+
+let previousPrice = '';
+const priceInterval = setInterval(() => {
+	const floatArea = document.querySelector('.float-cart-btn');
+	const currentPrice = getCurrentPrice();
+	if (floatArea && previousPrice !== currentPrice) {
+		previousPrice = currentPrice;
+		const floatAreaBtn = floatArea.querySelector('button');
+		floatAreaBtn.innerHTML = `${currentPrice} - Add To Cart`;
 	}
 }, 10);
