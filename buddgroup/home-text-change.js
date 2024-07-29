966bcf9fa93e0da2ef97f029acc452b00115bbe2
Tareq <tareqnmd@gmail.com>
2024-07-29 10:53:26 +0600

    product price

---
+++ buddgroup/home-text-change.js
@@ -0,0 +1,17 @@
+const interval = setInterval(() => {
+	try {
+		const topBtn = document.querySelector(
+			'.navbar .button.nav-button.trigger.w-button'
+		);
+		const heroBtn = document.querySelector(
+			'.wrapper.ban .button1.trigger.w-button'
+		);
+		if (topBtn && !document.querySelector('.new-elm')) {
+			topBtn.innerText = 'Request a Quote';
+			heroBtn.innerText = 'Request a Quote';
+			clearInterval(interval);
+		}
+	} catch (error) {
+		console.log('error', error);
+	}
+}, 10);
